a618b68da90f9c54ba2b90dab607210e87364c27
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2020-04-11 11:48:13 +0100

    Climbing stopwatch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { FaSun, FaRegMoon } from "react-icons/fa";
+import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+  }
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function resetStopwatch() {
+    setSeconds(0.0);
+    setIsActive(false);
+  }
+
+  function addCounter() {
+    setCounter(counter + 1);
+  }
+
+  function substractCounter() {
+    setCounter(counter - 1);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 0.01);
+      }, 10);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${darkMode ? "dark" : ""}`}>
+      <Container>
+        <Row>
+          <Col>
+            <div className="dark-mode-control d-flex flex-row-reverse">
+              <Button
+                className="transparent"
+                variant="primary"
+                onClick={toggleDarkMode}
+              >
+                {darkMode ? <FaSun /> : <FaRegMoon fill="#333333" />}
+              </Button>
+            </div>
+          </Col>
+        </Row>
+        <Row className="main">
+          <Col className="col-md-8 col-12">
+            <Row>
+              <div className="time w-100 d-flex justify-content-center">
+                <h1 style={{ fontSize: "10rem" }}>{seconds.toFixed(0)}</h1>
+                <h1 style={{ fontSize: "3rem", marginTop: "100px" }}>
+                  {(seconds % 1).toFixed(2).split(".")[1]}
+                </h1>
+              </div>
+            </Row>
+            <Row>
+              <div className="actions w-100 d-flex justify-content-center">
+                <Button onClick={toggle} size="lg" className="circular-xl mx-2">
+                  {isActive ? "Pause" : "Start"}
+                </Button>
+                <Button
+                  variant={darkMode ? "outline-light" : "outline-secondary"}
+                  onClick={resetStopwatch}
+                  size="lg"
+                  className="circular-xl mx-2"
+                >
+                  Reset
+                </Button>
+              </div>
+            </Row>
+          </Col>
+          <Col className="col-md-4 col-12 mt-5">
+            <div className="counter w-100 d-flex justify-content-center mt-5">
+              <Row>
+                <div className="mt-4">
+                  <Button
+                    size="lg"
+                    className="circular mx-3"
+                    onClick={substractCounter}
+                    disabled={counter === 0}
+                  >
+                    <span>-</span>
+                  </Button>
+                </div>
+                <div>
+                  <h1 style={{ fontSize: "5rem" }}>{counter}</h1>
+                </div>
+                <div className="mt-4">
+                  <Button
+                    size="lg"
+                    className="circular mx-3"
+                    onClick={addCounter}
+                  >
+                    <span>+</span>
+                  </Button>
+                </div>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
